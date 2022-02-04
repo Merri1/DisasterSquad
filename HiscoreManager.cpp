26e703ec02debda0318072bb678d9dec4b9f053b
DominikGodlewski <63593162+DominikGodlewski@users.noreply.github.com>
2022-02-04 15:02:26 +0000

    Highscore manager filled with some code that needs to be changed

--- HiscoreManager.cpp
+++ HiscoreManager.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 
+int hiScore;
 
 HiscoreManager::HiscoreManager()
 {
@@ -11,5 +12,46 @@ HiscoreManager::HiscoreManager()
 
 void writeToFile()
 {
+    std::ifstream file;
+    file.open("my_file.txt");
 
+    if (!file) 
+    {
+        std::cout << "File not created!";
+    }
+    else 
+    {
+        std::cout << "File created successfully!";
+        //file << playerScore;
+        file.close();
+    }
+    
+
+}
+
+void parseFile()//Load file to data structure
+{
+    std::ifstream file("assets/Hiscore.txt");
+    std::string str;
+    std::string file_contents;
+    while (std::getline(file, str))
+    {
+        file_contents += str;
+        file_contents.push_back('\n');
+    }
+}
+
+int	compareScore(int playerScore)
+{
+    if (playerScore >= hiScore)
+    {
+        hiScore = playerScore;
+    }
+}
+
+void printScores()
+{
+    std::ofstream outputFile("assets/Hiscore.txt");
+    outputFile << hiScore;
+    outputFile.close();
 }
